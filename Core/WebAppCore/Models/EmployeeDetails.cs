dc1905ff1384ace12907d298685dfb2289410a9f
Arun Subburaj <Arun.Subburaj@KALELOGISTICS.IN>
2019-12-13 16:55:08 +0530

    Core changes

---
+++ Core/WebAppCore/Models/EmployeeDetails.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCore.Models
+{
+    public class Employees
+    {
+        public string EmployeeName { get; set; }
+        public int EmplyeeID { get; set; }
+        public int PhoneNo { get; set; }
+        public string Address { get; set; }
+        public int Sex { get; set; }
+        public string Email { get; set; }
+        public int City { get; set; }
+        
+    }
+}
